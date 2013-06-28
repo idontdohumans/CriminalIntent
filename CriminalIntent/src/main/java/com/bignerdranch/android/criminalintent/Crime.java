6af9b556651a6b3d4b2ea80694d04098d4941d05
Donald <donald@thehoefer.com>
2013-06-28 11:12:34 -0400

    Finished Chapter 7.

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -6,7 +6,21 @@ import java.util.UUID;
  * Created by panda on 6/28/13.
  */
 public class Crime {
+
+    public UUID getId() {
+        return mId;
+    }
+
     private UUID mId;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
     private String mTitle;
 
     public Crime() {
