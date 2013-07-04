f55059f6260b1ef3aa1df644c6ddaed88d9dfa34
Donald <donald@thehoefer.com>
2013-07-04 14:37:03 -0400

    Added method toJSON() to Crime class.
    utilized in ...JSONSerializer.

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,6 +10,12 @@ import java.util.UUID;
  * Created by panda on 6/28/13.
  */
 public class Crime {
+
+    private static final String JSON_ID     = "id";
+    private static final String JSON_TITLE  = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE   = "date";
+
     private UUID mId;
     private String mTitle;
     private Date mDate;
@@ -44,6 +53,15 @@ public class Crime {
         // Generate unique identifier
         mId = UUID.randomUUID();
         mDate = new Date();
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_SOLVED, mSolved);
+        json.put(JSON_DATE, mDate.getTime());
+        return json;
     }
 
     @Override
