c45ae6e469cb6b64654338478a7588470db91f6f
Donald <donald@thehoefer.com>
2013-07-05 15:44:11 -0400

    Added DialogFragment to display larger image on thumbnail click.

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -38,6 +38,7 @@ public class CrimeFragment extends Fragment {
     private static final String DIALOG_DATE = "date";
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_PHOTO = 1;
+    private static final String DIALOG_IMAGE = "image";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -125,6 +126,21 @@ public class CrimeFragment extends Fragment {
         });
 
         mPhotoView = (ImageView)v.findViewById(R.id.crime_imageView);
+        mPhotoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Photo p = mCrime.getPhoto();
+                if (p == null)
+                    return;
+
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                String path = getActivity()
+                        .getFileStreamPath(p.getFilename()).getAbsolutePath();
+                ImageFragment.newInstance(path)
+                        .show(fm, DIALOG_IMAGE);
+            }
+        });
 
         // If camera is not available, disable camera functionality
         PackageManager pm = getActivity().getPackageManager();
